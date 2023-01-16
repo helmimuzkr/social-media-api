2eb6f26a7eef74b2bddbce49368eee4c389d901b
helmimuzkr <mhelmimuzakir@gmail.com>
2023-01-16 13:25:35 +0700

    feat: setup repository, service, and handler post feature

--- main.go
+++ main.go
@@ -3,7 +3,11 @@ package main
 import (
 	"log"
 	"social-media-app/config"
+	_postHandler "social-media-app/feature/post/handler"
+	_postRepository "social-media-app/feature/post/repository"
+	_postService "social-media-app/feature/post/service"
 
+	"github.com/go-playground/validator"
 	"github.com/labstack/echo"
 )
 
@@ -12,7 +16,16 @@ func main() {
 	db := config.OpenDB(c)
 	config.GormMigrartion(db)
 
+	v := validator.New()
+
+	// Setup feature
+	// Post
+	postRepo := _postRepository.NewPostRepository(db)
+	postSrv := _postService.NewPostService(postRepo, v)
+	postHandler := _postHandler.NewPostHandler(postSrv)
+
 	e := echo.New()
+
 	if err := e.Start(":8000"); err != nil {
 		log.Fatal(err)
 	}
