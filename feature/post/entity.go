f38691cc324478b24741c1ae2f4a6d61c550e97c
helmimuzkr <mhelmimuzakir@gmail.com>
2023-01-18 01:42:44 +0700

    feat: change contract include struct core

--- feature/post/entity.go
+++ feature/post/entity.go
@@ -1,37 +1,47 @@
 package post
 
 import (
+	"mime/multipart"
+
 	"github.com/labstack/echo"
 )
 
 type Core struct {
 	ID        uint
-	Caption   string
-	Image     string
+	Caption   string `validate:"required"`
+	Image     string `validate:"omitempty,url"`
 	Author    string
+	Avatar    string
 	CreatedAt string
+	UpdatedAt string
 }
 
 type PostHandler interface {
 	Create() echo.HandlerFunc
-	GetAll() echo.HandlerFunc
 	MyPost() echo.HandlerFunc
 	Update() echo.HandlerFunc
 	Delete() echo.HandlerFunc
+	GetByUserID() echo.HandlerFunc
+	GetByID() echo.HandlerFunc
+	GetAll() echo.HandlerFunc
 }
 
 type PostService interface {
-	Create(newPost Core) (Core, error)
-	GetAll() ([]Core, error)
+	Create(token interface{}, newPost Core, file *multipart.FileHeader) error
 	MyPost(token interface{}) ([]Core, error)
-	Update(token interface{}, updatePost Core) ([]Core, error)
-	Delete(token interface{}, postID int) error
+	Update(token interface{}, updatePost Core, file *multipart.FileHeader) error
+	Delete(token interface{}, postID uint) error
+	GetByUserID(userID uint) ([]Core, error)
+	GetByID(postID uint) ([]Core, error)
+	GetAll() ([]Core, error)
 }
 
 type PostRepository interface {
-	Create(newPost Core) (Core, error)
+	Create(userID uint, newPost Core) error
+	MyPost(userID uint) ([]Core, error)
+	Update(userID uint, postID uint, updatePost Core) error
+	Delete(userID uint, postID uint) error
+	GetByUserID(userID uint) ([]Core, error)
+	GetByID(postID uint) ([]Core, error)
 	GetAll() ([]Core, error)
-	MyPost(userID int) ([]Core, error)
-	Update(userID int, postID int, updatePost Core) ([]Core, error)
-	Delete(userID int, postID int) error
 }
