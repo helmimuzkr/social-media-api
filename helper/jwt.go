49b4a6481d554bbdf7dd2f3b9be680b61a53b9d6
helmimuzkr <mhelmimuzakir@gmail.com>
2023-01-17 13:08:06 +0700

    build: add function for generate and extract jwt token

--- helper/jwt.go
+++ helper/jwt.go
@@ -2,21 +2,16 @@ package helper
 
 import (
 	"log"
-	"social-media-app/config"
 
 	"github.com/golang-jwt/jwt/v4"
 )
 
 func GenerateToken(id uint) (string, error) {
-	// create claim
-	var claims jwt.MapClaims
-	claims["authorized"] = true
+	claims := jwt.MapClaims{}
 	claims["id"] = id
 
-	// create token
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
-	// generate token to string
 	strToken, err := token.SigningString()
 	if err != nil {
 		log.Println("Failed generate token", err)
@@ -26,21 +21,12 @@ func GenerateToken(id uint) (string, error) {
 	return strToken, nil
 }
 
-func ExtractToken(token string) (uint, error) {
-	res, err := jwt.Parse(token, func(t *jwt.Token) (interface{}, error) {
-		return []byte(config.JWT_KEY), nil
-	})
-	if err != nil {
-		log.Println("Failed extract token", err)
-		return 0, err
+func ExtractToken(t interface{}) int {
+	user := t.(*jwt.Token)
+	if user.Valid {
+		claims := user.Claims.(jwt.MapClaims)
+		userId := claims["userID"].(float64)
+		return int(userId)
 	}
-
-	if res.Valid {
-		claim := res.Claims.(jwt.MapClaims)
-		id := claim["id"].(float64)
-
-		return uint(id), nil
-	}
-
-	return 0, nil
+	return -1
 }
