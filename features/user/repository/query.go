cd017ca0731b791dd5f4112daccee536fe8af5bc
rizal-fahmi <rizalfahmi3002@gmail.com>
2023-01-18 15:48:37 +0700

    build: add register users

---
+++ features/user/repository/query.go
@@ -0,0 +1,49 @@
+package repository
+
+import (
+	"log"
+	"social-media-app/features/user"
+
+	"gorm.io/gorm"
+)
+
+type userQuery struct {
+	db *gorm.DB
+}
+
+func New(db *gorm.DB) user.UserRepository {
+	return &userQuery{
+		db: db,
+	}
+}
+
+func (uq *userQuery) RegisterRepo(newUser user.Core) (user.Core, error) {
+	cnv := CoreToUser(newUser)
+	err := uq.db.Create(&cnv).Error
+	if err != nil {
+		log.Println("Query create error", err.Error())
+		return user.Core{}, err
+	}
+	newUser.ID = cnv.ID
+	return newUser, nil
+}
+
+func (uq *userQuery) LoginRepo(email string) (user.Core, error) {
+	
+	return user.Core{}, nil
+}
+
+func (uq *userQuery) ProfileRepo(id uint) (user.Core, error) {
+
+	return user.Core{}, nil
+}
+
+func (uq *userQuery) UpdateRepo(id uint, updateUser user.Core) (user.Core, error) {
+	
+	return user.Core{}, nil
+}
+
+func (uq *userQuery) RemoveRepo(id uint) error {
+
+	return nil
+}
