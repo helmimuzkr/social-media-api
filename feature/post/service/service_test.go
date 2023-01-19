22bb356f36635bbea35379a37e5512f9273a24b9
helmimuzkr <mhelmimuzakir@gmail.com>
2023-01-19 15:26:48 +0700

    test: post unit test 61/100%

--- feature/post/service/service_test.go
+++ feature/post/service/service_test.go
@@ -3,6 +3,7 @@ package service
 import (
 	"errors"
 	"mime/multipart"
+	"os"
 	"social-media-app/feature/post"
 	"social-media-app/helper"
 	"social-media-app/mocks"
@@ -13,6 +14,18 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func DummyFile(path string) *multipart.FileHeader {
+	image, _ := os.Open(path)
+	fileheader := &multipart.FileHeader{
+		Filename: image.Name(),
+	}
+
+	return fileheader
+}
+
+// ---------------------
+// Add post
+// ---------------------
 func TestAddPost(t *testing.T) {
 	v := validator.New()
 	repo := mocks.NewPostRepository(t)
@@ -24,19 +37,14 @@ func TestAddPost(t *testing.T) {
 	// 		Image:    "acak.jpg",
 	// 		PublicID: "acak",
 	// 	}
-	// 	repo.On("Create", 1, input).Return(nil).Once()
-
-	// 	// body := bytes.NewBuffer([]byte{12, 11, 32, 31, 98})
-	// 	// mw := multipart.NewWriter(body)
-	// 	// file, _ := mw.CreateFormFile("image", "image.jpg")
+	// 	repo.On("Create", mock.Anything).Return(nil).Once()
 
 	// 	strToken, _ := helper.GenerateToken(1)
 	// 	token := helper.ValidateToken(strToken)
-	// 	file := &multipart.FileHeader{
-	// 		Filename: "acak.jpg",
-	// 		Size:     10,
-	// 	}
-	// 	err := srv.Create(token, post.Core{Caption: input.Caption}, file)
+
+	// 	image := DummyFile("./file-test/image.png")
+
+	// 	err := srv.Create(token, post.Core{Caption: input.Caption}, image)
 
 	// 	assert.Nil(t, err)
 	// })
@@ -75,6 +83,9 @@ func TestAddPost(t *testing.T) {
 	})
 }
 
+// ---------------------
+// Get My Post
+// ---------------------
 func TestMyPost(t *testing.T) {
 	v := validator.New()
 	repo := mocks.NewPostRepository(t)
@@ -119,7 +130,6 @@ func TestMyPost(t *testing.T) {
 		assert.NotNil(t, err)
 		assert.EqualError(t, err, "token invalid")
 		assert.Nil(t, actual)
-
 	})
 
 	t.Run("Post not found", func(t *testing.T) {
@@ -159,35 +169,273 @@ func TestMyPost(t *testing.T) {
 	})
 }
 
+// ---------------------
+// Update Post
+// ---------------------
 func TestUpdate(t *testing.T) {}
 
-// func TestDelete(t *testing.T) {
-// 	v := validator.New()
-// 	repo := mocks.NewPostRepository(t)
-// 	srv := NewPostService(repo, v)
+// ---------------------
+// Delete Post
+// ---------------------
+func TestDelete(t *testing.T) {
+	v := validator.New()
+	repo := mocks.NewPostRepository(t)
+	srv := NewPostService(repo, v)
 
-// 	t.Run("Success delete post", func(t *testing.T) {
-// 		resRepo := post.Core{
-// 			ID:        2,
-// 			Caption:   "Pegel",
-// 			Image:     "https://res.cloudinary.com/dnji8pgyl/image/upload/v1674031689/file/post/zsj8rcaj2p5mlhdacqgd.jpeg",
-// 			PublicID:  "file/post/zsj8rcaj2p5mlhdacqgd",
-// 			Author:    "Muzakir",
-// 			CreatedAt: "2023-01-18 15:48:07.441 +0700 WIB",
-// 			UpdatedAt: "2023-01-18 15:48:07.441 +0700 WIB",
-// 		}
-// 		repo.On("GetByID", uint(2)).Return(resRepo, nil).Once()
+	resRepo := post.Core{
+		ID:        2,
+		Caption:   "Pegel",
+		Image:     "https://res.cloudinary.com/dnji8pgyl/image/upload/v1674031689/file/post/zsj8rcaj2p5mlhdacqgd.jpeg",
+		PublicID:  "file/post/zsj8rcaj2p5mlhdacqgd",
+		Author:    "Muzakir",
+		CreatedAt: "2023-01-18 15:48:07.441 +0700 WIB",
+		UpdatedAt: "2023-01-18 15:48:07.441 +0700 WIB",
+	}
 
-// 		repo.On("Delete", uint(1), uint(2)).Return(nil).Once()
+	t.Run("Success delete post", func(t *testing.T) {
+		repo.On("GetByID", uint(2)).Return(resRepo, nil).Once()
 
-// 		strToken, _ := helper.GenerateToken(1)
-// 		token := helper.ValidateToken(strToken)
-// 		err := srv.Delete(token, uint(2))
+		repo.On("Delete", uint(1), uint(2)).Return(nil).Once()
 
-// 		assert.Nil(t, err)
-// 	})
-// }
+		strToken, _ := helper.GenerateToken(1)
+		token := helper.ValidateToken(strToken)
+		err := srv.Delete(token, uint(2))
 
+		assert.Nil(t, err)
+	})
+
+	t.Run("Error token invalid", func(t *testing.T) {
+		token := &jwt.Token{}
+		err := srv.Delete(token, 1)
+
+		assert.NotNil(t, err)
+		assert.EqualError(t, err, "token invalid")
+	})
+
+	t.Run("Get public id failed", func(t *testing.T) {
+		repo.On("GetByID", uint(2)).Return(post.Core{}, errors.New("not found")).Once()
+
+		strToken, _ := helper.GenerateToken(1)
+		token := helper.ValidateToken(strToken)
+		err := srv.Delete(token, uint(2))
+
+		assert.NotNil(t, err)
+		assert.EqualError(t, err, "post not found")
+	})
+
+	t.Run("Get public id failed internal server", func(t *testing.T) {
+		repo.On("GetByID", uint(2)).Return(post.Core{}, errors.New("database error")).Once()
+
+		strToken, _ := helper.GenerateToken(1)
+		token := helper.ValidateToken(strToken)
+		err := srv.Delete(token, uint(2))
+
+		assert.NotNil(t, err)
+		assert.EqualError(t, err, "internal server error")
+	})
+
+	t.Run("Delete post error", func(t *testing.T) {
+		repo.On("GetByID", uint(2)).Return(resRepo, nil).Once()
+
+		repo.On("Delete", uint(1), uint(2)).Return(errors.New("not found")).Once()
+
+		strToken, _ := helper.GenerateToken(1)
+		token := helper.ValidateToken(strToken)
+		err := srv.Delete(token, uint(2))
+
+		assert.NotNil(t, err)
+		assert.EqualError(t, err, "post not found")
+	})
+
+	t.Run("Delete post server error", func(t *testing.T) {
+		repo.On("GetByID", uint(2)).Return(resRepo, nil).Once()
+
+		repo.On("Delete", uint(1), uint(2)).Return(errors.New("database error")).Once()
+
+		strToken, _ := helper.GenerateToken(1)
+		token := helper.ValidateToken(strToken)
+		err := srv.Delete(token, uint(2))
+
+		assert.NotNil(t, err)
+		assert.EqualError(t, err, "internal server error")
+	})
+	// t.Run("Destroy image failed", func(t *testing.T) {
+	// 	repo.On("GetByID", uint(2)).Return(post.Core{}, nil).Once()
+
+	// 	repo.On("Delete", uint(1), uint(2)).Return(nil).Once()
+
+	// 	strToken, _ := helper.GenerateToken(1)
+	// 	token := helper.ValidateToken(strToken)
+	// 	err := srv.Delete(token, uint(2))
+
+	// 	assert.NotNil(t, err)
+	// 	assert.EqualError(t, err, "failed to delete image")
+	// })
+
+}
+
+// ---------------------
+// Get Post By User ID
+// ---------------------
 func TestGetByUserID(t *testing.T) {
-	
+	v := validator.New()
+	repo := mocks.NewPostRepository(t)
+	srv := NewPostService(repo, v)
+
+	t.Run("Success list my post", func(t *testing.T) {
+		resRepo := []post.Core{
+			{
+				ID:        1,
+				Caption:   "hallo semua",
+				Image:     "https://res.cloudinary.com/dnji8pgyl/image/upload/v1674031689/file/post/zsj8rcaj2p5mlhdacqgd.png",
+				Author:    "John Doe",
+				Avatar:    "https://res.cloudinary.com/dnji8pgyl/image/upload/v1674031689/file/avatar/zsj8rcaj2p5mlhdacqgd.jpeg",
+				CreatedAt: "2023-01-18 15:48:07.441 +0700 WIB",
+				UpdatedAt: "2023-01-18 15:48:07.441 +0700 WIB",
+			},
+			{
+				ID:        2,
+				Caption:   "Pegel",
+				Image:     "https://res.cloudinary.com/dnji8pgyl/image/upload/v1674031689/file/post/zsj8rcaj2p5mlhdacqgd.jpeg",
+				Author:    "John Doe",
+				Avatar:    "https://res.cloudinary.com/dnji8pgyl/image/upload/v1674031689/file/avatar/zsj8rcaj2p5mlhdacqgd.jpeg",
+				CreatedAt: "2023-01-18 15:48:07.441 +0700 WIB",
+				UpdatedAt: "2023-01-18 15:48:07.441 +0700 WIB",
+			},
+		}
+
+		repo.On("GetByUserID", uint(1)).Return(resRepo, nil).Once()
+
+		actual, err := srv.GetByUserID(uint(1))
+
+		assert.Nil(t, err)
+		for i := range resRepo {
+			assert.Equal(t, resRepo[i].ID, actual[i].ID)
+			assert.Equal(t, resRepo[i].Author, actual[i].Author)
+			assert.Equal(t, resRepo[i].Image, actual[i].Image)
+		}
+	})
+
+	t.Run("user not found", func(t *testing.T) {
+		repo.On("GetByUserID", uint(2)).Return(nil, errors.New("not found")).Once()
+
+		actual, err := srv.GetByUserID(uint(2))
+
+		assert.NotNil(t, err)
+		assert.EqualError(t, err, "user not found")
+		assert.Nil(t, actual)
+	})
+
+	t.Run("internal server server", func(t *testing.T) {
+		repo.On("GetByUserID", uint(2)).Return(nil, errors.New("database error")).Once()
+
+		actual, err := srv.GetByUserID(uint(2))
+
+		assert.NotNil(t, err)
+		assert.EqualError(t, err, "internal server error")
+		assert.Nil(t, actual)
+	})
+}
+
+// ---------------------
+// Get Post By User ID
+// ---------------------
+func TestGetByPostID(t *testing.T) {
+	v := validator.New()
+	repo := mocks.NewPostRepository(t)
+	srv := NewPostService(repo, v)
+
+	t.Run("Success get post by id", func(t *testing.T) {
+		resRepo := post.Core{
+			ID:        2,
+			Caption:   "Pegel",
+			Image:     "https://res.cloudinary.com/dnji8pgyl/image/upload/v1674031689/file/post/zsj8rcaj2p5mlhdacqgd.jpeg",
+			PublicID:  "file/post/zsj8rcaj2p5mlhdacqgd",
+			Author:    "Muzakir",
+			Avatar:    "https://res.cloudinary.com/dnji8pgyl/image/upload/v1674031689/file/avatar/zsj8rcaj2p5mlhdacqgd.jpeg",
+			CreatedAt: "2023-01-18 15:48:07.441 +0700 WIB",
+			UpdatedAt: "2023-01-18 15:48:07.441 +0700 WIB",
+		}
+
+		repo.On("GetByID", uint(2)).Return(resRepo, nil).Once()
+
+		actual, err := srv.GetByID(uint(2))
+
+		assert.Nil(t, err)
+		assert.Equal(t, resRepo.ID, actual.ID)
+		assert.Equal(t, resRepo.Avatar, actual.Avatar)
+	})
+
+	t.Run("not found", func(t *testing.T) {
+		repo.On("GetByID", uint(2)).Return(post.Core{}, errors.New("not found")).Once()
+
+		actual, err := srv.GetByID(uint(2))
+
+		assert.NotNil(t, err)
+		assert.EqualError(t, err, "post not found")
+		assert.Empty(t, actual)
+	})
+
+	t.Run("internal server error", func(t *testing.T) {
+		repo.On("GetByID", uint(2)).Return(post.Core{}, errors.New("database query error")).Once()
+
+		actual, err := srv.GetByID(uint(2))
+
+		assert.NotNil(t, err)
+		assert.EqualError(t, err, "internal server error")
+		assert.Empty(t, actual)
+	})
+}
+
+// ---------------------
+// Get All
+// ---------------------
+func TestGetAll(t *testing.T) {
+	v := validator.New()
+	repo := mocks.NewPostRepository(t)
+	srv := NewPostService(repo, v)
+
+	t.Run("Success list my post", func(t *testing.T) {
+		resRepo := []post.Core{
+			{
+				ID:        1,
+				Caption:   "hallo semua",
+				Image:     "https://res.cloudinary.com/dnji8pgyl/image/upload/v1674031689/file/post/zsj8rcaj2p5mlhdacqgd.png",
+				Author:    "John Doe",
+				Avatar:    "https://res.cloudinary.com/dnji8pgyl/image/upload/v1674031689/file/avatar/zsj8rcaj2p5mlhdacqgd.jpeg",
+				CreatedAt: "2023-01-18 15:48:07.441 +0700 WIB",
+				UpdatedAt: "2023-01-18 15:48:07.441 +0700 WIB",
+			},
+			{
+				ID:        2,
+				Caption:   "Pegel",
+				Image:     "https://res.cloudinary.com/dnji8pgyl/image/upload/v1674031689/file/post/zsj8rcaj2p5mlhdacqgd.jpeg",
+				Author:    "John Doe",
+				Avatar:    "https://res.cloudinary.com/dnji8pgyl/image/upload/v1674031689/file/avatar/zsj8rcaj2p5mlhdacqgd.jpeg",
+				CreatedAt: "2023-01-18 15:48:07.441 +0700 WIB",
+				UpdatedAt: "2023-01-18 15:48:07.441 +0700 WIB",
+			},
+		}
+
+		repo.On("GetAll").Return(resRepo, nil).Once()
+
+		actual, err := srv.GetAll()
+
+		assert.Nil(t, err)
+		for i := range resRepo {
+			assert.Equal(t, resRepo[i].ID, actual[i].ID)
+			assert.Equal(t, resRepo[i].Author, actual[i].Author)
+			assert.Equal(t, resRepo[i].Image, actual[i].Image)
+		}
+	})
+
+	t.Run("internal server server", func(t *testing.T) {
+		repo.On("GetAll").Return(nil, errors.New("database error")).Once()
+
+		actual, err := srv.GetAll()
+
+		assert.NotNil(t, err)
+		assert.EqualError(t, err, "internal server error")
+		assert.Nil(t, actual)
+	})
 }
