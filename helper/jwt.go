5c116a348efe6098e7fa60cd1921cdf46feda07b
helmimuzkr <mhelmimuzakir@gmail.com>
2023-01-19 09:28:32 +0700

    feat: update jwt

--- helper/jwt.go
+++ helper/jwt.go
@@ -2,17 +2,18 @@ package helper
 
 import (
 	"log"
+	"social-media-app/config"
 
-	"github.com/golang-jwt/jwt/v4"
+	"github.com/golang-jwt/jwt"
 )
 
 func GenerateToken(id uint) (string, error) {
 	claims := jwt.MapClaims{}
-	claims["id"] = id
+	claims["userID"] = id
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
-	strToken, err := token.SigningString()
+	strToken, err := token.SignedString([]byte(config.JWT_KEY))
 	if err != nil {
 		log.Println("Failed generate token", err)
 		return "", err
@@ -23,10 +24,25 @@ func GenerateToken(id uint) (string, error) {
 
 func ExtractToken(t interface{}) int {
 	user := t.(*jwt.Token)
+	userID := -1
 	if user.Valid {
 		claims := user.Claims.(jwt.MapClaims)
-		userId := claims["userID"].(float64)
-		return int(userId)
+		switch claims["userID"].(type) {
+		case float64:
+			userID = int(claims["userID"].(float64))
+		case int:
+			userID = claims["userID"].(int)
+		}
 	}
-	return -1
+
+	return userID
+}
+
+func ValidateToken(strToken string) *jwt.Token {
+	// Decode rawToken, parse from rawToken to jwt.Token
+	token, _ := jwt.Parse(strToken, func(t *jwt.Token) (interface{}, error) {
+		return []byte(config.JWT_KEY), nil
+	})
+
+	return token
 }
